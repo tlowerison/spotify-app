37fd7fb9593734d939cc90d16c17fbe62ad593f4
Trey Lowerison <tlowerison@berkeley.edu>
2017-12-27 17:00:14 -0800

    debug

--- public/scripts/services/factories/logIn.js
+++ public/scripts/services/factories/logIn.js
@@ -19,6 +19,7 @@ app.factory("logInFactory", function($location, $window, $http) {
 		}
 		function isLoggedIn() {
 			return new Promise(function(resolve, reject) {
+				console.log(spotifyHeaders)
 				if (spotifyHeaders != null) {
 					resolve();
 				} else {
